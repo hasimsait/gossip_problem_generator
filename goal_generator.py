3b3e8663dd268e30513ecc227057d946b81d3f1b
hasimsait <hasimsait@sabanciuniv.edu>
2021-01-05 20:16:13 +0300

    problem generator with uniformly distributed negative goals

---
+++ goal_generator.py
@@ -0,0 +1,58 @@
+import random
+
+def without_i(mylist,i):
+  newlist=[]
+  for element in mylist:
+    if element!=i:
+      newlist.append(element)
+  return newlist
+
+def generate_info(n):
+  agents={}
+  for i in range(n):
+    for j in range(n):
+      if i==j:
+        agents[i*n+i]=True
+      else:
+        agents[i*n+j]=False
+  #print(agents)
+  return agents
+
+def generate_constraints(n,info,percentage):
+  #basically generates a dictionary that represents what the info dict should look like in the end
+  if percentage!=0:
+    number_of_negative_goals_per_agent=int(n//(1/percentage))
+    #rounds the number of negative goals per agent (you said uniformly distributed negative goals are prefferable)
+    constraints={}
+    for i in range (n):
+      #for each agent
+      constraints[i]=random.sample(without_i(range(n),i), number_of_negative_goals_per_agent)
+      #select percentage*n agents (excluding i), for each agent i.
+  table=info
+  for key in table:
+    table[key]=True
+  if percentage==0:
+    return table
+  for i in range(n):
+    for cons in constraints[i]:
+      table[i*n+cons]=False
+  #print(table)
+  return table
+
+def generateKnows(n,percentage):
+  table=generate_constraints(n,generate_info(n),percentage)
+  tmp=''
+  for agent in range(n):
+    tmp+='            '
+    for secret in range(n):
+      if table[agent*n+secret]==True:
+        goalStr=f'(KNOWS agent{str(agent)} secret{str(secret)}) '
+      else:
+        goalStr=f'(not(KNOWS agent{str(agent)} secret{str(secret)})) '
+      tmp += goalStr
+    tmp+='\n'
+  return tmp
+
+
+
+
---
+++ problem_generator.py
@@ -0,0 +1,27 @@
+import sys
+from goal_generator import generateKnows
+
+try:
+    n=int(sys.argv[1])
+    percentage=float(sys.argv[2])
+except:
+    print("Couldn't read arguments.\ncall this script with \"$ python problem_generator.py n percentage\"\nn can be any integer, percentage is [0-1] float.")
+    quit()
+
+knows= generateKnows(n,percentage)
+
+
+outputStr='(define (problem problemGossip)\n    (:domain domainGossip)\n\n    (:objects\n        '
+for i in range(n):
+    outputStr+=f'agent{str(i+1)} '
+outputStr+='- agent\n        '
+for i in range(n):
+    outputStr+=f'secret{str(i+1)} '
+outputStr+='- secret\n    )\n\n    (:init\n'
+
+for i in range(n):
+    outputStr+=f'        (KNOWS agent{str(i+1)} secret{str(i+1)})\n'
+outputStr+='    )\n\n    (:goal\n        (and\n'
+outputStr+=knows
+outputStr+='        )\n    )\n)'
+print(outputStr)
