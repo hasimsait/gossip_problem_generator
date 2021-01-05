d8864fb12bffe8f5ac0887011cc7be4b52fa3bed
hasimsait <hasimsait@sabanciuniv.edu>
2021-01-05 20:43:40 +0300

    write to pddl file

--- problem_generator.py
+++ problem_generator.py
@@ -8,8 +8,13 @@ except:
     print("Couldn't read arguments.\ncall this script with \"$ python problem_generator.py n percentage\"\nn can be any integer, percentage is [0-1] float.")
     quit()
 
-knows= generateKnows(n,percentage)
+if len(sys.argv)>3:
+    #if an output filename is supplied
+    outputfile=str(sys.argv[3])
+else:
+    outputfile=f'problem_{str(n)}_{str(int(percentage*100))}.pddl'
 
+knows= generateKnows(n,percentage)
 
 outputStr='(define (problem problemGossip)\n    (:domain domainGossip)\n\n    (:objects\n        '
 for i in range(n):
@@ -25,3 +30,7 @@ outputStr+='    )\n\n    (:goal\n        (and\n'
 outputStr+=knows
 outputStr+='        )\n    )\n)'
 print(outputStr)
+
+f= open(outputfile,"w+")
+f.write(outputStr)
+f.close()
